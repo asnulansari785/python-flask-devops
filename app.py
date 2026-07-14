82ecd0f76f2c7b3770cd4637b71cd07987f9f9f7
Asnul Ansari <asnul.ansari@yahoo.com>
2026-07-14 23:04:49 +0530

    Render deployment support

--- app.py
+++ app.py
@@ -10,5 +10,14 @@ def home():
 def about():
     return render_template("about.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
+
+//if __name__ == "__main__":
+  //  app.run(host="0.0.0.0", port=5000, debug=True)
