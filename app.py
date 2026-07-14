72c3b87b12d1d3bc004b9c50afde08bc93d2874c
Asnul Ansari <asnul.ansari@yahoo.com>
2026-07-14 23:30:12 +0530

    Fix Python syntax for Render

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
         debug=False
     )
 
-//if __name__ == "__main__":
-  //  app.run(host="0.0.0.0", port=5000, debug=True)
+#if __name__ == "__main__":
+  #  app.run(host="0.0.0.0", port=5000, debug=True)
